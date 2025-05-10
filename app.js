e03851afd8c28bf93f4ef6bb8ea23290005e88de
Luiz Fernando <luizfranca35mc@gmail.com>
2025-05-09 22:28:49 -0300

    atualizacao

--- app.js
+++ app.js
@@ -2,3 +2,10 @@ console.log("ola2")
 let n1 = 7, n2 = 5.5, n3 = 2, n4 = 4
 let media = (n1 + n2 + n3 + n4) / 4
 console.log("A média é: " + media.toFixed(2))
+
+if(media>=5) {
+    console.log("parabens, voce passou")
+
+}else {
+    console.log("que pena, vc nao passou!")
+}
